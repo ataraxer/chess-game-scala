692a472d023188aa60902a765148cddbc45912a3
Anton Karamanov <ataraxer@gmail.com>
2014-03-04 00:49:51 +0400

    Draft Game object

--- src/main/scala/Game.scala
+++ src/main/scala/Game.scala
@@ -1,7 +1,30 @@
 package com.ataraxer.apps.chess.scala
 
+import com.ataraxer.apps.chess.scala.Color._
+
+
 object Game {
+  private var moves: List[Board] = List(Board())
+  private var turn: Color = White
+
+  private def lastMove = moves.head
+  private def addMove(move: Board) { moves ::= move }
+  private def switchTurn() { turn = turn.opposite }
+
+  def newGame() {
+    moves = List(Board())
+    turn = White
+  }
+
+
+  def move(from: Coord, to: Coord) {
+    addMove(lastMove.movePiece(from, to))
+    switchTurn()
+  }
+
+
   def main(args: Array[String]) {
     println("Ataraxer's Chess Game (Scala) v0.1.0")
+    newGame()
   }
 }
