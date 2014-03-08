b359a9275b2edd22827b0633056d1bbdc3feaa8f
Anton Karamanov <ataraxer@gmail.com>
2014-03-09 01:39:55 +0400

    Bind Move to board state

--- src/main/scala/Move.scala
+++ src/main/scala/Move.scala
@@ -3,28 +3,33 @@ package com.ataraxer.apps.chess.scala
 import com.ataraxer.apps.chess.scala.pieces._
 
 
-abstract class Move {
-  def apply(board: Board): Board
-  def unapply(board: Board): Board
+abstract class Move(board: Board) {
+  def apply: Board
+  def unapply: Board = board
 }
 
-case class SimpleMove(from: Coord, to: Coord) extends Move {
-  override def apply(board: Board): Board =
+
+case class SimpleMove
+  (board: Board)
+  (from: Coord, to: Coord)
+  extends Move(board)
+{
+  override def apply: Board =
     board(from).piece match {
       case Some(piece) => piece.move(board, from, to)
       case None => throw new Board.NoPieceAtCellException
     }
-
-  override def unapply(board: Board): Board =
-    SimpleMove(to, from).apply(board)
 }
 
-case class PromotePawn[A](from: Coord, to: Coord, newType: A) extends Move {
-  override def apply(board: Board): Board = {
-    val newBoard = SimpleMove(from, to).apply(board)
+
+case class PromotePawnMove[A]
+  (board: Board)
+  (from: Coord, to: Coord, newType: A)
+  extends Move(board)
+{
+  override def apply: Board = {
+    val newBoard = SimpleMove(board)(from, to).apply
     newBoard.promotePawn(to, newType)
   }
-
-  override def unapply(board: Board): Board = board
 }
 
