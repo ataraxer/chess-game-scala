1a38772743b4d13d0f1c740704e7447830c5dedc
Anton Karamanov <ataraxer@gmail.com>
2014-03-12 00:31:47 +0400

    Add castling move type

--- src/main/scala/Move.scala
+++ src/main/scala/Move.scala
@@ -1,6 +1,7 @@
 package com.ataraxer.apps.chess.scala
 
 import com.ataraxer.apps.chess.scala.pieces._
+import com.ataraxer.apps.chess.scala.Color._
 
 
 abstract class Move(board: Board) {
@@ -30,6 +31,31 @@ case class PromotePawnMove[A]
   override def apply: Board = {
     val newBoard = SimpleMove(board)(from, to).apply
     newBoard.promotePawn(to, newType)
+  }
+}
+
+
+case class CasltingMove(board: Board)(color: Color, short: Boolean = true)
+  extends Move(board)
+{
+  override def apply: Board = {
+    val row = color match {
+      case White => 0
+      case Black => 7
+    }
+
+    val bishopColumn = short match {
+      case true  => 7
+      case false => 0
+    }
+
+    val kingMove = SimpleMove(board)(
+      Coord(row, 4), Coord(row, bishopColumn)
+    ).apply
+
+    kingMove.update(
+      Cell(Coord(row, 7), Some(Bishop(color, true)))
+    )
   }
 }
 
